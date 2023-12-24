c2749190c797ff7bda70f1d302913d7d4c09cf93
Gabriel Silva Santos <gabrielss432112@gmail.com>
2023-12-24 18:24:45 -0300

    add terminal history

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent, KeyboardEvent, useEffect, useRef, useState } from 'react';
 
 const Terminal = () => {
   const [inputText, setInputText] = useState<string>('');
+  const [history, setHistory] = useState<string[]>([]);
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
   useEffect(() => {
@@ -23,7 +24,7 @@ const Terminal = () => {
 
   const handleKeyPress = (event: KeyboardEvent<HTMLTextAreaElement>) => {
     if (event.key === 'Enter') {
-      console.log(inputText);
+      setHistory((prevHistory) => [...prevHistory, inputText.trim()]);
       setInputText('');
     }
   };
@@ -39,11 +40,26 @@ const Terminal = () => {
         onChange={handleInputChange}
         onKeyDown={handleKeyPress}
       />
-      <span id='username'>
+      {/* <span id='username'>
         visitor@terminal.com:<span className='color-main-blue'>~</span>
         <span className='color-white'>$</span>
-      </span>{' '}
-      <span className='color-white'>{inputText}</span> <span id='typer'></span>
+      </span>
+      <span className='color-white'>{inputText}</span> <span id='typer'></span> */}
+      {/* Exibir histórico de comandos */}
+      {history.map((command, index) => (
+        <div key={index}>
+          <span className='username'>
+            visitor@terminal.com:<span className='color-main-blue'>~</span>
+            <span className='color-white'>$</span>
+          </span>
+          <span className='color-white'>{command}</span>
+        </div>
+      ))}
+      <span className='username'>
+        visitor@terminal.com:<span className='color-main-blue'>~</span>
+        <span className='color-white'>$</span>
+      </span>
+      <span className='color-white'>{!!inputText.length ? inputText.trim() : inputText}</span> <span id='typer'></span>
     </div>
   );
 };
