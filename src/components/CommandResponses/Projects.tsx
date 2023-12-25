46666e1f2880c2a1cf3d3990fade1dfbde70b207
Gabriel Silva Santos <gabrielss432112@gmail.com>
2023-12-25 19:27:05 -0300

    add Projects component

---
+++ src/components/CommandResponses/Projects.tsx
@@ -0,0 +1,83 @@
+const Projects = () => {
+  return (
+    <div className='response'>
+      <span className='color-main-yellow'>Here are some projects I did:</span>
+      <br />
+      <span className='color-main-blue'>
+        My personal portfolio ={' '}
+        <a
+          href='https://github.com/gabrielsantos7/PortFolio'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='color-white'
+        >
+          github.com/gabrielsantos7/PortFolio
+        </a>
+      </span>{' '}
+      <br />
+      <span className='color-main-blue'>
+        Box Shadow Generator ={' '}
+        <a
+          href='https://github.com/gabrielsantos7/box-shadow-generator'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='color-white'
+        >
+          github.com/gabrielsantos7/box-shadow-generator
+        </a>
+      </span>{' '}
+      <br />
+      <span className='color-main-blue'>
+        React Posts App ={' '}
+        <a
+          href='https://github.com/gabrielsantos7/posts-react'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='color-white'
+        >
+          github.com/gabrielsantos7/posts-react
+        </a>
+      </span>{' '}
+      <br />
+      <span className='color-main-blue'>
+        School Student API ={' '}
+        <a
+          href='https://github.com/gabrielsantos7/API-Alunos-Express'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='color-white'
+        >
+          github.com/gabrielsantos7/API-Alunos-Express
+        </a>
+      </span>{' '}
+      <br />
+      <span className='color-main-blue'>
+        Maromba's GYM Backend ={' '}
+        <a
+          href='https://github.com/gabrielsantos7/projeto-academia-backend'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='color-white'
+        >
+          github.com/gabrielsantos7/projeto-academia-backend
+        </a>
+      </span>{' '}
+      <br />
+      <span className='color-main-blue'>
+        Maromba's GYM Frontend ={' '}
+        <a
+          href='https://github.com/gabrielsantos7/projeto-academia-frontend'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='color-white'
+        >
+          github.com/gabrielsantos7/projeto-academia-frontend
+        </a>
+      </span>
+      <br /> <br />
+      <span>Visit my Github to find more cool projects!</span>
+    </div>
+  );
+};
+
+export default Projects;
