d8eb8bb69aadeb575a4133b1c7c89cfb32cc37ad
Gabriel Silva Santos <gabrielss432112@gmail.com>
2023-12-25 17:30:41 -0300

    add datetime

--- src/components/Username.tsx
+++ src/components/Username.tsx
@@ -1,11 +1,15 @@
+import { getCurrentTime } from "../helper";
 
 const Username = () => {
-  return(
-    <span className='username'>
-      visitor@terminal.com:<span className='color-main-blue'>~</span>
-      <span className='color-white'>$</span>
-    </span>
+  return (
+    <>
+      <span className='datetime color-white'>{getCurrentTime()}</span>
+      <span className='username'>
+        visitor@terminal.com:<span className='color-main-blue'>~</span>
+        <span className='color-white'>$</span>
+      </span>
+    </>
   );
-}
+};
 
 export default Username;
--- src/helper.ts
+++ src/helper.ts
@@ -3,3 +3,17 @@ import { commands } from './commands';
 export const findCommand = (input: string): string | undefined => {
   return commands.find((cmd) => cmd === input);
 };
+
+export const getCurrentTime = (): string => {
+  const now: Date = new Date();
+  let hours: number | string = now.getHours();
+  let minutes: number | string = now.getMinutes();
+  let seconds: number | string = now.getSeconds();
+
+  hours = (hours < 10 ? '0' : '') + hours;
+  minutes = (minutes < 10 ? '0' : '') + minutes;
+  seconds = (seconds < 10 ? '0' : '') + seconds;
+
+  const currentTime: string = `${hours}:${minutes}:${seconds}`;
+  return currentTime;
+};
